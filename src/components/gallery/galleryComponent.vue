5ef5733fdcfc241813440d4891c29005e8e62fbf
Viktor Puchkov <victor.p@wecandevelopit.com>
2018-12-10 19:06:44 +0300

    2

--- src/components/gallery/galleryComponent.vue
+++ src/components/gallery/galleryComponent.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
-    <div class="container">
+    <div class="container-">
       <h1>{{titleBlock}}</h1>
-      <p>{{images.text}}</p>
+      <div class="flex">
+        <div v-for="image in images" :key="image.id" class="ddd">
+          <img :src="image.imgSrc" :alt="image.imgAlt">
+        </div>
+      </div>
+      <p style="text-align: center;margin-top: 20px;">{{text}}</p>
     </div>
   </div>
 </template>
@@ -12,15 +17,43 @@ export default {
   name: "app-gallery",
   data: function () {
     return {
-      titleBlock: 'sldkfjglsdkjfglsdjlfgkjsd',
+      titleBlock: 'Gallery',
+      text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et accusantium a mollitia molestias praesentium voluptatum possimus, dicta assumenda veritatis, ut totam in provident reprehenderit repellendus placeat voluptatibus eaque quasi ipsum!',
       images: [
         {
-          'imgSrc': 'falsjdflaskjdflk',
-          'text': 'lorem  dlskajflkjasldjflasd asjdflas dasjdf asldfjas dflas dflas dfasldjf asdfhaskhdfa slsjkdf lasd'
+          'imgSrc': 'https://images.unsplash.com/photo-1491933382434-500287f9b54b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80',
+          'imgAlt': 'comp'
+        },
+        {
+          'imgSrc': 'https://images.unsplash.com/photo-1491933382434-500287f9b54b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80',
+          'imgAlt': 'comp'
+        },
+        {
+          'imgSrc': 'https://images.unsplash.com/photo-1491933382434-500287f9b54b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80',
+          'imgAlt': 'comp'
+        },{
+          'imgSrc': 'https://images.unsplash.com/photo-1491933382434-500287f9b54b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80',
+          'imgAlt': 'comp'
+        }
+        ,{
+          'imgSrc': 'https://images.unsplash.com/photo-1491933382434-500287f9b54b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80',
+          'imgAlt': 'comp'
+        },
+        {
+          'imgSrc': 'https://images.unsplash.com/photo-1491933382434-500287f9b54b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80',
+          'imgAlt': 'comp'
+        },
+        {
+          'imgSrc': 'https://images.unsplash.com/photo-1491933382434-500287f9b54b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80',
+          'imgAlt': 'comp'
+        },
+        {
+          'imgSrc': 'https://images.unsplash.com/photo-1491933382434-500287f9b54b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80',
+          'imgAlt': 'comp'
         }
       ]
     }
-  },
+  }
 };
 </script>
 
