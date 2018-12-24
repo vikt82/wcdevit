c0a70c839a36fce9150b767c9499fd8640aac679
Viktor Puchkov <victor.p@wecandevelopit.com>
2018-12-24 18:43:42 +0300

    portfolio

--- src/components/portfolio/portfolioComponent.vue
+++ src/components/portfolio/portfolioComponent.vue
@@ -1,12 +1,18 @@
 <template>
   <section class="portfolio portfolio-block">
     <div class="container">
-      <h2 class="title-section">Portfolio</h2>
+      <h2 class="title-section">{{titleBlock}}</h2>
     </div>
 
     <div class="portfolio-container">
+      <div v-for="(item, index) in portfolioBlock" :key="index" class="portfolio-item">
+        <figure>
+          <img :src="getPortfolioImg(index)" :srcset="item.thumbnailSrcset" :alt="item.thumbnailAlt">
+          <a v-bind:href="item.linkPortfolio" class="case-studies" data="Case Studies" target="_blank"></a>
+        </figure>
+      </div>
 
-      <div class="portfolio-item">
+      <!-- <div class="portfolio-item">
         <figure>
           <img src="./../../assets/image/portfolio/exim_flowers.png" alt>
           <a href="https://exim.flowers" class="case-studies" data="Case Studies" target="_blank"></a>
@@ -35,7 +41,7 @@
           <img src="./../../assets/image/portfolio/vessage.png" alt>
           <a href="http://vessage.com" class="case-studies" target="_blank"></a>
         </figure>
-      </div>
+      </div> -->
 
     </div>
   </section>
@@ -43,7 +49,52 @@
 
 <script>
 export default {
-  name: "app-portfolio"
+  name: "app-portfolio",
+  data: function() {
+    return {
+      titleBlock: "Portfolio",
+
+      portfolioBlock: [
+        {
+          thumbnailSrc: "exim_flowers.webp",
+          thumbnailAlt: "exim flowers",
+          linkPortfolio: "https://exim.flowers",
+          thumbnailSrcset: "exim_flowers@2x.png 2x"
+        },
+        {
+          thumbnailSrc: "vessage.png",
+          thumbnailAlt: "vessage",
+          linkPortfolio: "http://vessage.com",
+          thumbnailSrcset: "exim_flowers@2x.png 2x"
+        },
+        {
+          thumbnailSrc: "cargo_flowers.png",
+          thumbnailAlt: "cargo flowers",
+          linkPortfolio: "https://cargo.flowers",
+          thumbnailSrcset: "exim_flowers@2x.png 2x"
+        },
+        {
+          thumbnailSrc: "accuscore.png",
+          thumbnailAlt: "accuscore",
+          linkPortfolio: "http://accuscore.com",
+          thumbnailSrcset: "exim_flowers@2x.png 2x"
+        },
+        {
+          thumbnailSrc: "adoptanagent.png",
+          thumbnailAlt: "adoptanagent",
+          linkPortfolio: "https://www.adoptanagent.com",
+          thumbnailSrcset: "exim_flowers@2x.png 2x"
+        }
+      ]
+    };
+  },
+
+  methods: {
+    getPortfolioImg(index) {
+      return require("@/assets/image/portfolio/" + this.portfolioBlock[index].thumbnailSrc);
+    }
+  }
+
 };
 </script>
 
